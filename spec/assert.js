792d10fc3214188464e9e6eb744891784ce5ffc9
Freddie <fredjlawson@gmail.com>
2018-09-18 14:42:33 +0100

    adds pass message

--- spec/assert.js
+++ spec/assert.js
@@ -2,7 +2,9 @@ var assert = {
   isTrue: function(assertionToCheck) {
     if (!assertionToCheck) {
       throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
-    };
+    } else {
+      console.log('Test pass!')
+    }
   }
 };
 
