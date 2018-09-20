013290240c34a41b7c5810c6603cd1471dc6f602
Malachy Gilchrist <malachygilchrist@gmail.com>
2018-09-20 17:00:24 +0100

    adds note id test

--- spec/noteTest.js
+++ spec/noteTest.js
@@ -1,10 +1,14 @@
 
 var string = 'My favourite language is JavaScript'
-var testNote = new Note(string)
+var testNote = new Note(string, 1)
 
 
 function hasText() {
   assert.isTrue(testNote.text === string)
+}
+
+function hasID() {
+  assert.isTrue(testNote.id === 1)
 }
 
 hasText()
